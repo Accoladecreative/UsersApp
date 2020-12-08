d03fa21722cdbdf594b169bb9b2e90ea8dfbb838
ODUNAYOMI@Accolade <kolade4jesus10@gmail.com>
2020-12-08 14:28:27 +0100

    user detail initialize on user details activity

--- app/src/main/java/com/example/usersapp/ui/UserDetailsActivity.java
+++ app/src/main/java/com/example/usersapp/ui/UserDetailsActivity.java
@@ -2,13 +2,30 @@ package com.example.usersapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.usersapp.R;
+
 public class UserDetailsActivity extends AppCompatActivity {
+    String firstName, lastName, email,phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
+
+        Intent intent = getIntent();
+        if(intent != null){
+            firstName = intent.getStringExtra("firstName");
+            lastName = intent.getStringExtra("lastName");
+            email = intent.getStringExtra("email");
+            phone = intent.getStringExtra("phone");
+        }
     }
+
+
+
+
+
 }
