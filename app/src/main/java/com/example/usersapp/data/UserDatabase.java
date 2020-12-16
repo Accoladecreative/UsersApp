0de4fa6b7238103beaae02147d8d5c41a7c6453d
ODUNAYOMI@Accolade <kolade4jesus10@gmail.com>
2020-12-16 04:23:18 +0100

    Revert "my gym app demo"
    This reverts commit a11faaa3

--- app/src/main/java/com/example/usersapp/data/UserDatabase.java
+++ app/src/main/java/com/example/usersapp/data/UserDatabase.java
@@ -67,6 +67,15 @@ public abstract class UserDatabase extends RoomDatabase {
             userDao.insertUser(new User("Timi", "Ibidun", "ibidun@gmail.com", "03-07-2000","098765698769","10-12-2000"));
             userDao.insertUser(new User("Tomiwa", "Ibidun", "tommy@gmail.com", "10-12-2000","98767876578","21-09-2009"));
 
+            userDao.insertUser(new User("Femi", "Oluwadare", "femi@gmail.com","03-07-2000", "08132747502","10-12-2000"));
+            userDao.insertUser(new User("Tope", "Oluwadare", "tope@gmail.com", "03-07-2000","07036927791","10-12-2000"));
+            userDao.insertUser(new User("Kolade", "Oluwadare", "kolade@gmail.com","", "08104757514","10-12-2000"));
+            userDao.insertUser(new User("Titi", "Oluwadare", "titi@gmail.com","03-07-2000", "08164029533",""));
+            userDao.insertUser(new User("Taiwo", "Akanle", "taiwo@gmail.com", "","08198272627","10-12-2000"));
+            userDao.insertUser(new User("Kenny", "Akanle", "kenny@gmail.com","03-07-2000", "897765467687","10-12-2000"));
+            userDao.insertUser(new User("Timi", "Ibidun", "ibidun@gmail.com", "03-07-2000","098765698769","10-12-2000"));
+            userDao.insertUser(new User("Tomiwa", "Ibidun", "tommy@gmail.com", "10-12-2000","98767876578","21-09-2009"));
+
 
             return null;
         }
